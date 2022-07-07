cdd7c3b04c7b6dbfdd05182fe32f183beeccded1
Royer Reyes León <royerleon7@gmail.com>
2022-07-07 13:58:04 -0500

    search, table components

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+    selector: 'app-por-pais',
+    templateUrl: './por-pais.component.html',
+    styleUrls: ['./por-pais.component.css']
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+    termino: string = '';
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    buscar() {
+        console.log(this.termino);
+
+    }
 
 }
